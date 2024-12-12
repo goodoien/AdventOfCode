f5ae924e31b78f16808a51e5a21ec491372341bc
goodoien <8219443+goodoien@users.noreply.github.com>
2024-12-12 16:17:54 -0700

    Update day-2.lua

--- 2024/lua/day-2.lua
+++ 2024/lua/day-2.lua
@@ -9,9 +9,9 @@ do
     local ascending_reports = {}
     local descending_reports = {}
     local unsafe_reports = {}
-    local unknown_reports = {}
     local safe_reports = {}
     local unsafe_count = 0
+    local safe_count = 0
     local file = io.open("reports.txt", "r")
     assert(file ~= nil, "file was nil")
 
@@ -24,12 +24,28 @@ do
     end
     io.close(file)
 
+    -- check for single element lines
+    for index, value in ipairs(lines) do
+        if #value < 2 then
+            print("only one elements found")
+            safe_reports["line_"..tostring(value)] = value
+        end
+    end
+
+    -- check for lines with only two elements
+    for index, value in ipairs(lines) do
+        if #value == 2 and math.abs(value[1] - value[2]) >= 1  and math.abs(value[1] - value[2]) <= 3 then
+            print("only two elements found")
+            safe_reports["line_"..tostring(value)] = value
+        end
+    end
+
     -- ascending_reports
     for line, report in ipairs(lines) do
         for index, value in ipairs(report) do
             if index == #report then
                 table.insert(ascending_reports, report)
-                unsafe_reports["line_"..tostring(report)] = nil
+                unsafe_reports["line_"..tostring(report)] = ""
                 break
             end
             if value > report[index + 1] then
@@ -43,7 +59,7 @@ do
         for index, value in ipairs(report) do
             if index == #report then
                 table.insert(descending_reports, report)
-                unsafe_reports["line_"..tostring(report)] = nil
+                unsafe_reports["line_"..tostring(report)] = ""
                 break
             end
             if value < report[index + 1] then
@@ -57,8 +73,8 @@ do
     for index, value in ipairs(ascending_reports) do
         for i, report in ipairs(value) do
             if i == #value then
-                table.insert(safe_reports, value)
-                unsafe_reports["line_"..tostring(value)] = nil
+                safe_reports["line_"..tostring(value)] = value
+                unsafe_reports["line_"..tostring(value)] = ""
                 break
             end
             local differnce = value[i + 1] - report
@@ -77,8 +93,8 @@ do
     for index, value in ipairs(ascending_reports) do
         for i, report in ipairs(value) do
             if i == #value then
-                table.insert(safe_reports, value)
-                unsafe_reports["line_"..tostring(value)] = nil
+                safe_reports["line_"..tostring(value)] = value
+                unsafe_reports["line_"..tostring(value)] = ""
                 break
             end
             local differnce =  report - value[i + 1]
@@ -94,9 +110,15 @@ do
     end
 
     for key, value in pairs(unsafe_reports) do
-        if value ~= nil then
-            print("unsfe for line: ".. key)
+        if value ~= "" then
+            -- print("unsfe for line: ".. key)
             unsafe_count = unsafe_count + 1
+        end
+    end
+    for key, value in pairs(safe_reports) do
+        if value ~= "" then
+            -- print("unsfe for line: ".. key)
+            safe_count = safe_count + 1
         end
     end
 
@@ -104,5 +126,5 @@ do
     print("Total asceding reports found: " .. #ascending_reports)
     print("Total desceding reports found: " .. #descending_reports)
     print("Total unsafe reports found: " .. unsafe_count)
-    print("Total safe reports found: " .. #safe_reports)
+    print("Total safe reports found: " .. safe_count)
 end
